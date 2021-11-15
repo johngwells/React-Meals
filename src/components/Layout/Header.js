5cc6c38a1ad1a7652433d29bb8e0c4ecd455a6fb
John Wells <molekewl2017@gmail.com>
2021-11-14 23:08:34 -0800

    ui layout | create components/css & display dummy data

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import mealsImage from "../../assets/meals.jpeg";
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = () => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['header-image']}>
+        <img alt="A table full of food" src={mealsImage} />
+      </div>
+    </>
+  );
+};
+
+export default Header;
