8ba2375dfbe8f1763d8b86acaf5175f5568189b0
John Wells <molekewl2017@gmail.com>
2021-11-15 17:32:10 -0800

    create modal overlay with portals to the top of the dom tree & UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <>
-      <Header />
-      <main>
-        <Meals />
-      </main>
+      <Cart>
+        <Header />
+        <main>
+          <Meals />
+        </main>
+      </Cart>
     </>
   );
 }
