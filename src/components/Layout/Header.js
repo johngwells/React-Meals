4bd4da44b0c09f9f5008e5b765a93e738adfb775
John Wells <molekewl2017@gmail.com>
2021-11-16 16:21:25 -0800

    hide/show modal

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,12 @@ import mealsImage from "../../assets/meals.jpeg";
 import classes from './Header.module.css';
 import HeaderCartButton from './HeaderCartButton';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['header-image']}>
         <img alt="A table full of food" src={mealsImage} />
